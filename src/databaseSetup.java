a4c04ac405b313a02858d132516da2ffa30537ee
Yijun Pan Stautland <yijun@staut.land>
2016-11-29 01:41:59 +0100

    Written displayMountains method, getConnection method.

--- src/databaseSetup.java
+++ src/databaseSetup.java
@@ -1,7 +1,31 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class databaseSetup {
 	
+	private static Connection con;
+	private static boolean hasData = false;
 	
+	public ResultSet displayMountains() throws SQLException, ClassNotFoundException
+	{
+		if (con == null)
+			getConnection();
+		
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery("SELECT * FROM MOUNTAIN");
+		
+		return res;		
+	}
+
+	private void getConnection() throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		
+		Class.forName("org.sqlite.JDBC");
+		con = DriverManager.getConnection("jdbc:sqlite:Mountains.db");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
