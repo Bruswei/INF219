4a333f84fb001840d267c8cdd0bc2b4d3a720139
Yijun Pan Stautland <yijun@staut.land>
2016-12-01 19:24:41 +0100

    initialize method for databaseSetup written. Now it will search for mountain.db, if not found create one.

--- src/databaseSetup.java
+++ src/databaseSetup.java
@@ -15,16 +15,51 @@ public class databaseSetup {
 			getConnection();
 		
 		Statement state = con.createStatement();
-		ResultSet res = state.executeQuery("SELECT * FROM MOUNTAIN");
+		ResultSet res = state.executeQuery("SELECT * FROM mountain");
 		
 		return res;		
 	}
 
-	private void getConnection() throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
+	private void getConnection() throws ClassNotFoundException, SQLException 
+	{
 		
 		Class.forName("org.sqlite.JDBC");
 		con = DriverManager.getConnection("jdbc:sqlite:Mountains.db");
+		initialize();
+	}
+
+	private void initialize() throws SQLException 
+	{
+		// IF database has data then we do not need to initialize
+		if (!hasData)
+		{
+			hasData = true;
+			
+			Statement state = con.createStatement();
+			ResultSet res = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='mountain'");
+			if (!res.next())
+				System.out.print("Database or mountain table not found, building database with fixed talbes!");
+			
+			// Building the table
+			Statement state2 = con.createStatement();
+			state2.execute("CREATE TABLE mountain (M_ID INTEGER PRIMARY KEY," 
+							+ "Height INTEGER," + "PromFactor INTEGER,"
+							+ "Name TEXT," + "Location TEXT,"
+							+ "Difficulity TEXT," + "PicAdress TEXT)");
+			
+			state2.execute("CREATE TABLE attributes (M_ID INTEGER," + "attribute TEXT,"
+							+ "AValue TEXT," + "FOREIGN KEY(M_ID) REFERENCES mountain(M_ID)");
+			
+			state2.execute("CREATE TABLE trip (M_ID INTEGER," + "T_ID INTEGER PRIMARY KEY," 
+							+ "date REAL," + "ShortSummary TEXT," + "Summary TEXT,"
+							+ "FOREIGN KEY(M_ID) REFERENCES mountain(M_ID))");
+			
+			state2.execute("CREATE TABLE resources (T_ID INTEGER," + "comments TEXT,"
+						   + "address TEXT," + "FOREIGN KEY(T_ID) REFERENCES trip(T_ID))");
+			
+			// TODO: insert data from jason scan:
+		}
+		
 	}
 
 	public static void main(String[] args) {
