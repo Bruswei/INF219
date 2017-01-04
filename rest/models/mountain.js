9a4ab680a40c2a7c8df6afa750dfd22ca8c84cad
INF112v16-yst003 <yijun@staut.land>
2017-01-04 15:53:01 +0100

    Mountains.js is now updated with schema function.

--- rest/models/mountain.js
+++ rest/models/mountain.js
@@ -4,4 +4,15 @@ var restful = require('node-restful');
 
 // Sqlite3 database
 var sqlite3 = require('sqlite3').verbose();
-var db = './../Mountains.db';
+var db = '../Mountains.db';
+
+// Schema
+var mountainSchema = new sqlite3.Schema({
+    name: String,
+    id: Number,
+    height: Number,
+    pf: Number,  
+});
+
+// Return model
+module.exports = restful.model('mountains', mountainSchema);
